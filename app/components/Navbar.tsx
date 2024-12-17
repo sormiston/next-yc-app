3af4389dc5d6fe83595ff518bb356dbb3972f874
sormiston <sean.david.ormiston@gmail.com>
2024-12-17 12:54:22 +0000

    Root layout with NavBar + Github OAuth flow

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import React from "react";
+import { auth, signIn, signOut } from "@/auth";
+import Link from "next/link";
+import logo from "@public/yc-directory-logo.png";
+
+async function Navbar() {
+  const authUser = await auth();
+
+  return (
+    <header className="p-1 md:px-3 md:py-2 lg:px-5 lg:py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Image
+          src={logo}
+          alt="YC Directory logo"
+          priority={true}
+          className="w-36"
+        />
+
+        {authUser?.user ? (
+          <div className="flex items-center h-9 gap-2 md:gap-5 lg:gap-8">
+            <Link href="/startup/create">Create</Link>
+            <form
+              action={async () => {
+                "use server";
+                await signOut({ redirectTo: "/" });
+              }}
+            >
+              <button type="submit">Logout</button>
+            </form>
+            {/* TODO: this avatar must serve as link to /users/[userId] */}
+            <Image
+              src={authUser.user.image || ""}
+              width={460}
+              height={460}
+              alt={`${authUser.user.name}'s Avatar`}
+              placeholder="blur"
+              blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNcJgkAAWsAwZyjJXAAAAAASUVORK5CYII="
+              className="w-8 h-8 lg:w-9 lg:h-9 rounded-full"
+            />
+          </div>
+        ) : (
+          <form
+            action={async () => {
+              "use server";
+              await signIn();
+            }}
+          >
+            <button type="submit">Login</button>
+          </form>
+        )}
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "",
+        pathname: "/u/**",
+        // search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
